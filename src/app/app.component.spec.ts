afd241b4ddcd5a8fe0e375dcd5e6f8730a72ea99
flexpartha <partha.chakra2@gmail.com>
2023-05-25 14:09:17 +0530

    code commited

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,26 +2,62 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  //const fixtureComponent = TestBed.createComponent(AppComponent);
+  //const appComponent = fixtureComponent.componentInstance;
+   //pretestItems;
+   //operationalItems;
+
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
+    declarations: [AppComponent],
   }));
 
-  it('should create the app', () => {
+  it("should return an array with the same length as the input array of pretestItems", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const result = app.pretestItems;
+    expect(result.length).toEqual(app.pretestItems.length);
   });
 
-  it(`should have as title 'AssignmentWithTDD'`, () => {
+  it("should return an array with the same length as the input array of operationalItems", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AssignmentWithTDD');
+    const result = app.operationalItems;
+    expect(result.length).toEqual(app.operationalItems.length);
   });
 
-  it('should render title', () => {
+  it("should select 2 pretest items and place them at the beginning of the array", () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AssignmentWithTDD app is running!');
+    const app = fixture.componentInstance;
+    const resultfirst2item = app.firstTwoItems;
+    const resnextEightItems = app.nextEightItems;
+    const finalResult = resultfirst2item.concat(resnextEightItems);
+    const radomItem = app.randomizedItems ;
+    expect(finalResult).toEqual(radomItem);
   });
+
+  it("array should shuffle in random way", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    let testlet = [
+      'Pretest Item 1', 'Pretest Item 2', 'Pretest Item 3', 
+      'Operational Item 1', 'Operational Item 2', 'Operational Item 3', 'Operational Item 4', 'Operational Item 5', 'Operational Item 6',
+      'Pretest Item 4'
+    ];
+    const result = app.randomizedItems;
+    expect(result).not.toEqual(testlet);
+    expect(result.length).toEqual(testlet.length);
+    
+  });
+
+  it("should shuffle array are truely  in random way", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    let testlet = [
+      'Pretest Item 1', 'Pretest Item 2', 'Pretest Item 3', 
+      'Operational Item 1', 'Operational Item 2', 'Operational Item 3', 'Operational Item 4', 'Operational Item 5', 'Operational Item 6',
+      'Pretest Item 4'
+    ];
+    expect(testlet).not.toEqual(app.randomizedItems)
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AssignmentWithTDD';
+
+   pretestItems = ['Pretest Item 1', 'Pretest Item 2', 'Pretest Item 3', 'Pretest Item 4'];
+   operationalItems = ['Operational Item 1', 'Operational Item 2', 'Operational Item 3', 'Operational Item 4', 'Operational Item 5', 'Operational Item 6'];
+
+   firstTwoItems = this.getRandomItems(this.pretestItems, 2);
+   
+   remainingPretestItems = this.pretestItems.filter(item => !this.firstTwoItems.includes(item));
+
+   remainingItems = this.remainingPretestItems.concat(this.operationalItems);
+
+   nextEightItems = this.getRandomItems(this.remainingItems, 8);
+
+   randomizedItems = this.firstTwoItems.concat(this.nextEightItems);
+
+   getRandomItems(array: any[], count: any) {
+    const shuffled = array.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
+  }
 }
